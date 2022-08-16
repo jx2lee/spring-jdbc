e3f54127b6cb33e5d31f2b1fe8bcce66b6e5be32
jx2lee <63435794+jx2lee@users.noreply.github.com>
2022-08-16 21:53:56 +0900

    Revert "add part-1 (#1)"
    This reverts commit 91b1b033a84e86f4d912dce521c05035aba17db8.

--- deep-dive-jdbc/src/main/java/io/github/jx2lee/deepdivejdbc/connection/ConnectionConst.java
+++
@@ -1,7 +0,0 @@
-package io.github.jx2lee.deepdivejdbc.connection;
-
-public abstract class ConnectionConst {
-    public static final String URL ="jdbc:h2:tcp://192.168.64.6:1521/spring-jdbc-01";
-    public static final String USERNAME ="sa";
-    public static final String PASSWORD ="";
-}
